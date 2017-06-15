67eda289e4275abafa8a26b2a9e2329a99ca723b
joshNM <josh@northernmediauk.com>
2017-06-15 09:23:14 +0100

    edit home-intro to add wordpress content

--- templates/partial-home-intro.php
+++ templates/partial-home-intro.php
@@ -2,20 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-7">
-                <h1>Welcome to Teachwell</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <?php while(have_posts()) : the_post(); ?>
+                <?php the_content(); ?>
                 <a href="" class="Button Button--primary Button--large">About us</a>
+                <?php endwhile; ?>
             </div>
             <div class="col-sm-12 col-md-5">
                 <h2>What is it?</h2>
